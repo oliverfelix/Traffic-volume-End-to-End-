2f17dc4ff357cbae1ff7b61fbd478eaff57118d7
oliverfelix <jayandhans484@gmail.com>
2024-03-13 17:44:56 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Trafficvol import logger
 from Trafficvol.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Trafficvol.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,5 +13,16 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
